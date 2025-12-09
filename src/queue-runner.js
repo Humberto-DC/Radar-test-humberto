7a1fe1a7f930dface9a6d747dcf5fe77315f6405
YasminDayrell <yasminstudy2@gmail.com>
2025-12-09 18:04:32 -0300

    deleting useless file

--- src/queue-runner.js
+++ src/queue-runner.js
@@ -1,6 +1,7 @@
 setInterval(async () => {
   try {
-    const resp = await fetch("http://localhost:3000/api/queue/worker");
+    // const resp = await fetch("http://localhost:3000/api/queue/worker");
+    const resp = await fetch("http://172.16.0.155:80/api/queue/worker");
     const json = await resp.json();
     console.log("[worker]", json);
   } catch (e) {
